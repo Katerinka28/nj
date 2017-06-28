fdfb9da3fc8ec1477e5168463939b1c0680ced51
Katerinka28 <katuxadmitr@gmail.com>
2017-06-28 09:23:10 +0300

    28june9-23

--- dist/scripts/bundle.min.js
+++ dist/scripts/bundle.min.js
@@ -10,5 +10,19 @@ function initMap() {
         });
       }
  $("#form").validate();
+ $("#newform").validate();
+
+
+$(document).ready(function(){
+   //mobile menu 
+    $('.header__mob-icon').click(function(e) {
+        $('.header__mobile').toggleClass('is-mobile-header');
+        $('body').toggleClass('load');
+    });
+    $('.header__mob-close').click(function(e) {
+        $('.header__mobile').removeClass('is-mobile-header');
+        $('body').removeClass('load');
+    });
+});
 
 // 39.833851, -74.871826.
